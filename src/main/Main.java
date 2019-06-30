953d1e6ffcb0531f123f90d7bf99a04da99a86af
Aleksandra Ponochovna <aleksandra_ponochovna@outlook.com>
2019-06-30 21:37:25 +0300

    first commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,13 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
+import servlets.RequestServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        RequestServlet requestServlet = new RequestServlet();
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(requestServlet), "/*");
         Server server = new Server(8080);
         server.setHandler(context);
         System.out.println("Server started.");
